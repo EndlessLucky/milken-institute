f1e5c26084e5283aacf5ccdf76d51f830a565ea7
JamesAlexander-rockwall <james.alexander.rockwall@gmail.com>
2020-12-18 17:34:32 +0200

    feat - implement show json data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button, Collapse, Tabs, Tab } from 'react-bootstrap';
-import React, {useState} from 'react';
+import React, {useState, map} from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -141,29 +141,26 @@ class App extends React.Component {
               {
                 menuObject.map((obj, i) => {
                   return (
-                    <Tab key={i} eventKey="obj.tab_name" title="obj.tab_name">
+                    <Tab key={i} eventKey={obj.tab_name} title={obj.tab_name}>
                       <div className="row">
                         {
-                          menuObject.menu_columns_array.map((obj, i) => {
+                          obj.menu_columns_array.map( (obj, i) => {
                             return(
                               <div className="col-lg-3 col-sm-12" key={i}>
                                 <h5 className="menu-title">{obj.column_name}</h5>
                                 <ul className="menu-content">
                                   {
-                                    
+                                    obj.links_array.map((obj, i) => {
+                                      return(
+                                        <li key={i}>
+                                          <h6>
+                                            <a href="">{obj.title}</a>
+                                          </h6>
+                                          <p className="hidden-sm hidden-xs">{obj.description}</p>
+                                        </li>
+                                      )
+                                    })
                                   }
-                                  <li>
-                                    <h6>
-                                      <a href="">About Us</a>
-                                    </h6>
-                                    <p className="hidden-sm hidden-xs">Learn about our mission</p>
-                                  </li>
-                                  <li>
-                                    <h6>
-                                      <a href="">Leadership</a>
-                                    </h6>
-                                    <p className="hidden-sm hidden-xs">Meet the Milken Institute leadership team</p>
-                                  </li>
                                 </ul>
                               </div>
                             )
@@ -174,28 +171,6 @@ class App extends React.Component {
                   )
                 })
               }
-              
-              <Tab eventKey="profile" title="Profile">
-                <div className="row">
-                  <div className="col-lg-3 col-sm-12">
-                    <h5 className="menu-title">Asia</h5>
-                    <ul className="menu-content">
-                      <li>
-                        <h6>
-                          <a href="">About Us</a>
-                        </h6>
-                        <p className="hidden-sm hidden-xs">Learn about our mission</p>
-                      </li>
-                      <li>
-                        <h6>
-                          <a href="">Leadership</a>
-                        </h6>
-                        <p className="hidden-sm hidden-xs">Meet the Milken Institute leadership team</p>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </Tab>
             </Tabs>          
           </div>
         </Collapse>
